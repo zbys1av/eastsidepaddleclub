0d143617ac1362392fb2a2da5868da046e285d57
zbys1av <zbyslav13@gmail.com>
2023-09-09 07:49:49 +0300

    scrolling + bugs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.scss';
+
+// let bgPattern = document.getElementById('test');
+// window.addEventListener("scroll", function(){
+//     console.log(+window.pageYOffset);
+//     bgPattern.style = +window.pageYOffset;
+// });
