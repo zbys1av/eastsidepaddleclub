0e9f0994cf04029a402f4c869bfed6db8f44dea1
zbys1av <zbyslav13@gmail.com>
2023-09-16 06:13:14 +0300

    bugs/load animation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import './style.scss';
 
-// let bgPattern = document.getElementById('test');
-// window.addEventListener("scroll", function(){
-//     console.log(+window.pageYOffset);
-//     bgPattern.style = +window.pageYOffset;
-// });
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        const comunity__title = entry.target.querySelector('.comunity__title');
+        // const comunity__text = entry.target.querySelector('.comunity__text');
+
+        if(entry.isIntersecting){
+            comunity__title.classList.add('text-animation');
+            // comunity__text.classList.add('text-animation');
+
+            return;
+        }
+
+        comunity__title.classList.remove('text-animation');
+        // comunity__text.classList.remove('text-animation');
+    });
+});
+
+observer.observe(document.querySelector('.comunity'));
